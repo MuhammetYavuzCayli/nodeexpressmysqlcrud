14687b82b193bab6092a6950fea1d5157ba99396
Yavuz ÇAYLI <muhammetyavuzcayli@gmail.com>
2017-08-05 16:24:30 +0300

    gerekli aciklamalar eklendi

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
+
+//indirilen ve kullanacağımız js de tanımlanan kütüphaneler
 var express = require("express");
 var mysql = require("mysql");
+//
+
+//Mysql bağlantısı için gerekli config ve pool kısmı
 var pool = mysql.createPool({
-    connectionLimit: 1000,
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'denemeDB'
+    connectionLimit: 1000, //kaç thread e kadar cevap vermesi gerektiğini belirttiğimiz Limit değeri bu ÖNEMLİ!!
+    host: 'localhost', //lokalde çalışıyorsanız
+    user: 'root', //varsayılan olarak root der sanıyorum mysqlciler
+    password: 'password', //burası veritabanı şifren
+    database: 'databasename' //bağlandığın veritabanı adı
 });
 var app = express();
-
+//Uygulamanın çalışacağı localhost portu
+//uygulama http://localhost:8080 de çalışacak
 app.listen(8080);
-
+//veritabanın da değişiklikler yapmak için gerekli izinler
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
     next();
 });
-
-function handle_database(req, res, query) {
+//query leri çalıştıracak fonksiyon
+function queryExecuter(req, res, query) {
 
     pool.getConnection(function (err, connection) {
         if (err) res.send({ "code": 100, "status": "Error in connection database", "Error": err.message });
@@ -35,25 +41,13 @@ function handle_database(req, res, query) {
         });
     });
 }
-
+//Users tablosundan select sorgusu ve http://localhost:8080/user adresindeki görüntüsü 
 app.get("/user", function (req, res) {
     var query = "select * from users";
-    handle_database(req, res, query);
+    queryExecuter(req, res, query);
 });
+//Users tablosundan 3 tane satır döndürmesi için gerekli select sorgusu ve http://localhost:8080/top3 adresindeki görüntüsü 
 app.get("/top3", function (req, res) {
     var query = "select * from users LIMIT 3";
-    handle_database(req, res, query);
-});
-app.get("/post", function (req, res) {
-    var query = "select * from post";
-    handle_database(req, res, query);
-});
-
-app.get("/follower", function (req, res) {
-    var query = "select * from follower where UID = 1";
-    handle_database(req, res, query);
-});
-app.get("/complexquery", function (req, res) {
-    var query = "select FullName,PhotoURL,PostImageURL,PostText,PostType,Timestamp,LikeCount,CommentCount,ShareCount from post as p INNER JOIN users as u ON p.UID = u.UID INNER JOIN follower as f ON p.UID = f.followerID WHERE f.UID = 1  ORDER BY Timestamp asc";
-    handle_database(req, res, query);
+    queryExecuter(req, res, query);
 });
